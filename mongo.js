a01b60a60bef47049566431bff3330adf77d6c9b
Yonatan Boehm <yonatanboehm@Yonatans-MacBook-Pro.local>
2023-11-11 19:15:47 +0200

    exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstackopen:${password}@cluster0.jcj9yqe.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+    Person.find({}).then(result => {
+        console.log('Phonebook:')
+        result.forEach(person => {
+        console.log(person.name, " ", person.number)
+        })
+        mongoose.connection.close()
+    })
+}
+else {
+    const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+    })
+
+    person.save().then(result => {
+    console.log('person saved!')
+    mongoose.connection.close()
+    })
+}
